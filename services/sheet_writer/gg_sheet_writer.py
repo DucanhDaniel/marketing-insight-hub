975a36f85a460b979c615e87330e3c34660a8b06
Phạm Đức Anh <ducanh2002add@gmail.com>
2026-03-07 15:04:40 +0700

    rmv raw value input in sw

--- services/sheet_writer/gg_sheet_writer.py
+++ services/sheet_writer/gg_sheet_writer.py
@@ -430,8 +430,7 @@ class GoogleSheetWriter:
             return 0
 
         worksheet = self._get_or_create_worksheet(sheet_name)
-        has_image_col = 'product_img' in headers or 'creative_thumbnail_url' in headers
-        value_input = 'USER_ENTERED' if has_image_col else 'RAW'
+        value_input = 'USER_ENTERED'
 
         total_rows = len(data_to_write)
         total_written = 0
